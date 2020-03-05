ff272a7ef9d9c8608eca00a3a3195a6ceaeec37d
Luke Sherwood <luke.sherwood55@gmail.com>
2020-03-05 05:36:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,6 +9,18 @@ class Student
     student.grade = row[2]
     student
   end
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      where grade = 10
+      LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
   
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
